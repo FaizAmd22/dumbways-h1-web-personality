4edf456f3b822ab81bb9e6527e4d96e9ba2f312d
Faizhal Ahmad <ahmadfaizhal8@gmail.com>
2023-11-21 20:58:07 +0700

    Menambahkan day10

---
+++ day10/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('/testimonials', (req, res) => {
+    res.json([
+        {
+            author: "Mr. White",
+            content: "Let him cook!",
+            image: "https://i.pinimg.com/564x/f9/85/de/f985debe6c7a2e7a7b630bffc1548e07.jpg",
+            rating: 4,
+        },
+        {
+            author: "Professor",
+            content: "I cannot teach anybody anything; I can only make them think.",
+            image: "https://i.pinimg.com/564x/e2/72/bf/e272bfec9361ae5dee632b7b9137c5a8.jpg",
+            rating: 3
+        },
+        {
+            author: "Sherlock Holmes",
+            content: "The game is on!",
+            image: "https://i.pinimg.com/564x/02/10/fd/0210fd848228fab719b22995e9271b2b.jpg",
+            rating: 5
+        },
+        {
+            author: "Gustavo Fring",
+            content: "Say my name",
+            image: "https://i.pinimg.com/564x/f1/2a/93/f12a938a9a516e026632ee1f461e6a79.jpg",
+            rating: 4
+        },
+        {
+            author: "Jim Moriarty",
+            content: "Did you miss me?",
+            image: "https://i.pinimg.com/564x/98/54/13/985413a19b3c08a42b4862aa8b05aefb.jpg",
+            rating: 5
+        }
+    ])
+})
+
+app.listen(port, () => {
+    console.log(`Server berjalan di port ${port}`)
+})
